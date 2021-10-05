65894698203f62990050d461d4530a088cbd1ab6
Ricardo <rr155914@gmail.com>
2021-10-05 16:23:21 -0500

    Add bug

--- person.rb
+++ person.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 class Person
   attr_accessor :age, :name
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
+    @id = Random.rand(1..1)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -22,3 +24,5 @@ person = Person.new(22, 'Richie')
 
 p person.of_age?
 p person.can_use_services?
+
+binding.pry
