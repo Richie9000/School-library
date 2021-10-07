c713476e08b5b98f32568392452070ff46f73526
Ricardo <rr155914@gmail.com>
2021-10-07 18:10:20 -0500

    Add rental class

--- book.rb
+++ book.rb
@@ -1,6 +1,13 @@
 class Book
-  def initialize
+attr_accessor :title, :author, :rentals   
+
+  def initialize (title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, person, self)
   end
 end
